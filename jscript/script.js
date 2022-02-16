e5058ed2102cededb83d077d7d89c47b0e30f97b
Uenderss <meajudabarto@gmail.com>
2022-02-16 20:51:11 -0300

    abrir tela 2 em CSS

--- jscript/script.js
+++ jscript/script.js
@@ -7,8 +7,8 @@ promise.catch(erroObterQuizzes);
 
 function obterQuizzes(resposta) {
      //Deve ser criado um filtro para ver se o id do usuario é diferente dos demais para então exibir a lista de quizzes ;
-     let todosQuizzes, meusQuizzes;
-     renderizarQuizzes(resposta.data,".seus-quizzes");   
+    //  let todosQuizzes, meusQuizzes;
+    //  renderizarQuizzes(resposta.data,".seus-quizzes");   
      renderizarQuizzes(resposta.data,".todos-quizzes");
      
 }
@@ -21,10 +21,10 @@ function renderizarQuizzes(lista, container){
     
     for(let i=0;i<lista.length;i++){
         
-        let montarQuizz =`<div class="quizz-item" onclick="QuizzSelecionado(this)"
-        style="background-image: linear-gradient(
-            180deg
-            , rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url('${lista[i].image}')"
+        let montarQuizz =`<div class="quizz-item" onclick="quizzSelecionado(this)"
+        style="background-image: 
+            linear-gradient( 180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%),
+             url('${lista[i].image}')"
         >
         <span class="titulo-quizz">
         
@@ -35,5 +35,25 @@ function renderizarQuizzes(lista, container){
 
                                  
     }
+    
+    quizzes = document.querySelectorAll(".quizz-item");
+    
+    // for(let i=0;i<quizzes.length;i++){
+    //     quizzes[i].style.backgroundImage = `url('${lista[i].image}')`;
+    // }
+}
+let teste;
+function quizzSelecionado(selecionado){
+    teste = selecionado;
+    document.body.innerHTML += `<div class="abrir-quiz">
+    <section>
+            <div class="banner">
+                <h2 class="abrir-quiz-titulo">${selecionado.querySelector("span").innerText}</h2>
+            </div>
+    </section>
+    </div>`;
+
+  
+
     
 }
