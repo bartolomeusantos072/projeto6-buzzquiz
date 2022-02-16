4393e24e91b2a7f5f131414a7df35994f34a3329
Uenderss <meajudabarto@gmail.com>
2022-02-16 16:38:43 -0300

    listar quizzes fornecidos pelo servidor

--- jscript/script.js
+++ jscript/script.js
@@ -17,10 +17,16 @@ function renderizarQuizzes(lista){
     // console.log(resposta.data);
     console.log(lista[1]);
     for(let i=0;i<lista.length;i++){
-        let montarQuizz =`<div class="quizz-item">
-        ${lista[i].title} `;
+        let montarQuizz =`<div class="quizz-item"><span class="titulo-quizz">
+        ${lista[i].title}</span> `;
         montarQuizz+=`<div>`;
-        document.querySelector(".todos-quizzes").innerHTML+=montarQuizz;  
+        document.querySelector(".todos-quizzes").innerHTML+=montarQuizz;
+        document.querySelector(".quizz-item").style.backgroundImage = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${lista[i].image})`;
+        
+    
+                       
+                        
+                
     }
 }
 
