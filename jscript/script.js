99892f8577ded29f80b44e6c1a346ac7e02a45fc
Uenderss <meajudabarto@gmail.com>
2022-02-16 18:27:07 -0300

    listar quizzes fornecidos pelo servidor com imgem

--- jscript/script.js
+++ jscript/script.js
@@ -1,42 +1,39 @@
-let quizzes=[];
+let idsDoUsuario=[];
+
 
 const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
 promise.then(obterQuizzes);
 promise.catch(erroObterQuizzes);
 
 function obterQuizzes(resposta) {
-     quizzes = resposta.data;
-     renderizarQuizzes(resposta.data);
+     //Deve ser criado um filtro para ver se o id do usuario é diferente dos demais para então exibir a lista de quizzes ;
+     let todosQuizzes, meusQuizzes;
+     renderizarQuizzes(resposta.data,".seus-quizzes");   
+     renderizarQuizzes(resposta.data,".todos-quizzes");
      
 }
 function erroObterQuizzes(error){
     alert.error(error);
 }
-
-function renderizarQuizzes(lista){
-    // console.log(resposta.data);
-    console.log(lista[1]);
+let quizzes=[];
+function renderizarQuizzes(lista, container){
+   
+    
     for(let i=0;i<lista.length;i++){
-        let montarQuizz =`<div class="quizz-item"><span class="titulo-quizz">
+        
+        let montarQuizz =`<div class="quizz-item" onclick="QuizzSelecionado(this)"
+        style="background-image: linear-gradient(
+            180deg
+            , rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url('${lista[i].image}')"
+        >
+        <span class="titulo-quizz">
+        
         ${lista[i].title}</span> `;
         montarQuizz+=`<div>`;
-        document.querySelector(".todos-quizzes").innerHTML+=montarQuizz;
-        document.querySelector(".quizz-item").style.backgroundImage = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${lista[i].image})`;
+        document.querySelector(container).innerHTML+=montarQuizz;
         
-    
-                       
-                        
-                
+
+                                 
     }
+    
 }
-
-// // criarQuiz(0);
-// // let contador = 0;
-
-// // function criarQuiz(num) {
-
-// //     if (num > 0) {
-// //         contador++;
-// //     }
-// //     console.log(contador);
-// // }
