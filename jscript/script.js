fd45ab64bd1d049a81fbbaa2d13fc3ec2f782ae7
Uenderss <meajudabarto@gmail.com>
2022-02-16 15:52:46 -0300

    implementacao do quizz tela 1 parte inferior

--- jscript/script.js
+++ jscript/script.js
@@ -1,18 +1,36 @@
-const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-promise.then(obterQuizzes)
+let quizzes=[];
 
+const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+promise.then(obterQuizzes);
+promise.catch(erroObterQuizzes);
 
 function obterQuizzes(resposta) {
-    console.log(resposta.data);
+     quizzes = resposta.data;
+     renderizarQuizzes(resposta.data);
+     
+}
+function erroObterQuizzes(error){
+    alert.error(error);
 }
 
-criarQuiz(0);
-let contador = 0;
-
-function criarQuiz(num) {
-
-    if (num > 0) {
-        contador++;
+function renderizarQuizzes(lista){
+    // console.log(resposta.data);
+    console.log(lista[1]);
+    for(let i=0;i<lista.length;i++){
+        let montarQuizz =`<div class="quizz-item">
+        ${lista[i].title} `;
+        montarQuizz+=`<div>`;
+        document.querySelector(".todos-quizzes").innerHTML+=montarQuizz;  
     }
-    console.log(contador);
 }
+
+// // criarQuiz(0);
+// // let contador = 0;
+
+// // function criarQuiz(num) {
+
+// //     if (num > 0) {
+// //         contador++;
+// //     }
+// //     console.log(contador);
+// // }
