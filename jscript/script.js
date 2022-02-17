fb6ec854fb883fbfd75a5d5e02228f95ced8fca4
Uenderss <meajudabarto@gmail.com>
2022-02-17 18:05:11 -0300

    remover  arquivo js para tela 2

--- jscript/script.js
+++ jscript/script.js
@@ -48,3 +48,57 @@ function renderizarQuizzes(lista, container) {
 }
 
 
+
+
+function quizzSelecionado(selecionado, num) {
+    
+
+    urlImage = selecionado.style.backgroundImage.split('"')[1];
+
+    document.body.innerHTML += `<div class="abrir-quiz">
+    <section>
+            <div class="banner" style=" background-image: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${urlImage})">
+                <h2 class="abrir-quiz-titulo">${selecionado.querySelector("span").innerText}</h2>
+            </div>
+            <div class="todos-games">
+            </div>
+    </section>
+    </div>`;
+    quizzesJogos(num);
+
+}
+
+
+function quizzesJogos(num) {
+
+    for (let i = 0; i < quizzesGame[num].questions.length; i++) {
+
+        let montarQuizz = `<div class="jogo">
+                                <div class="unico-jogo">
+                                        <span class="titulo-quizz" style="
+                                        background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5), ${quizzesGame[num].questions[i].color})">${quizzesGame[num].questions[i].title}</span>
+                                    <div class="imagens-respostas" onclick="">`;
+                                    let answers = quizzesGame[num].questions[i].answers;                  
+                                        answers.sort();
+                                        answers.forEach(answer => {
+                                            montarQuizz += `
+                                                <div class="imagem"> <img src=${ answer.image}/> <span class="resposta"><h3>${answer.text}</h3></span></div>`;
+                                                
+                                                
+                                        });
+                    
+            montarQuizz +=`         </div>
+                                </div>                
+                            </div>`;
+
+        document.querySelector(".todos-games").innerHTML += montarQuizz;
+
+    }
+
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+
