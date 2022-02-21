554180bc724ef0b59e1643d58d656d748beeea79
Uenderss <meajudabarto@gmail.com>
2022-02-21 06:25:00 -0300

    implemantacao das validacoes das perguntas e niveis do quizz

--- jscript/scriptTela3.js
+++ jscript/scriptTela3.js
@@ -1,8 +1,13 @@
+const QUANTIDADEPERGUNTAS =0,
+      TAMANHOMINIMO=20,
+      TAMANHOMAXIMO=65;
+
 let novoQuizz = { 
+    id:null,
     title: null,
     image:null,
-    questions:{},
-    levels:{},
+    questions:[{ title:null, color:null, answers:{text:null, image:null, isCorrectAnswers:false}}],
+    levels:{title:null,image:null},
 };
 
 
@@ -35,7 +40,14 @@ function validarPasso1() {
     let qtde_pergunta = document.getElementById("qtde_pergunta_quizz");
     let nivel = document.getElementById("qtde_nivel_quizz");
 
-    if (titulo.value.length < 20 || titulo.value.length > 65) {
+    /*Para teste valido de passo 1 */
+    titulo.value ="Nayane Thalyta e Bartolomeu";
+    url_imagem.value= "https://upload.wikimedia.org/wikipedia/pt/a/ac/Vegeta.jpg";
+    qtde_pergunta.value = 2;
+    nivel.value =2;
+
+
+    if (titulo.value.length < TAMANHOMINIMO || titulo.value.length > TAMANHOMAXIMO) {
         alert("O Titulo de quizz Invalido tente novamente");
         titulo.value="";
         titulo.focus();
@@ -47,7 +59,7 @@ function validarPasso1() {
         return;
     }
 
-    if(qtde_pergunta.value < 3){
+    if(qtde_pergunta.value < QUANTIDADEPERGUNTAS){
         alert("A quantidade de pergunta deve ser no mínimo 3!!");
         qtde_pergunta.value =3;
         qtde_pergunta.focus();
@@ -60,7 +72,6 @@ function validarPasso1() {
         nivel.focus();
         return;
     }
-    // alert("Tudo ok");
     
     novoQuizz.title = titulo.value;
     novoQuizz.image = url_imagem.value;
@@ -79,13 +90,16 @@ function validarPasso1() {
     <img src="" id="image">
   */
   
-    criarPerguntas(qtde_pergunta.value, nivel.value);
+    criarPerguntas(qtde_pergunta.value , nivel.value);
 
 }
 
 
 let teste;
 function criarPerguntas(qtde,niveis) {
+     console.log("Não usei ainda"+niveis);
+     let respostasIncorretas=[];
+
 
      let montarQuestoes = `<div  class="criar-perguntas">
     <section>
@@ -108,38 +122,45 @@ function criarPerguntas(qtde,niveis) {
                 
                 <input class="inputs" type="text" id="perguntas_quizz" placeholder="Texto da pergunta" required>
                 
-                <input class="inputs" type="text" id="cor_quizz" placeholder="Cor de Fundo da pergunta" required>
+                <div class="inputs"><input class="cores" type="text" id="cor_quizz" placeholder="Cor de Fundo da pergunta" required=""><input type="color" id="favcolor" name="favcolor" value="#ff0000"></div>
                 
                 <span class="subtitulo-formulario"><h5>Resposta correta</h5></span>
-
-                <input class="inputs" type="text" id="resposta_ok" placeholder="Resposta correta" required>
-                <input class="inputs" type="text" id="url_imagem_resposta_1" placeholder="URL da imagem" required>
-
-                <span class="subtitulo-formulario"><h5>Resposta incorretas</h5></span>
-                <input class="inputs" type="text" id="resposta_incorreta_1" placeholder="Resposta incorreta 1" required>
-                <input class="inputs" type="text" id="url_imagem_resposta_2" placeholder="URL da imagem 1" required>
-
-                <input class="inputs" type="text" id="tx_resposta_incorreta_2" placeholder="Resposta incorreta 2" required>
-                <input class="inputs" type="text" id="url_imagem_resposta_3" placeholder="URL da imagem 2" required>
-
-                <input class="inputs" type="text" id="resposta_incorreta_3" placeholder="Resposta incorreta 3" required>
-                <input class="inputs" type="text" id="url_imagem_resposta_4" placeholder="URL da imagem 3" required>
+                 <div class="alternativa-true">       
+                    <input class="inputs" type="text" id="resposta_ok" placeholder="Resposta correta" required>
+                    <input class="inputs" type="text" id="url_imagem_resposta_1" placeholder="URL da imagem" required>
+                </div>
+                <span class="subtitulo-formulario"><h5>Resposta incorretas</h5></span>`;
+                
+                //  respostasIncorretas[i]=2;
+                respostasIncorretas[i]= prompt("Quantas repostas erradas para a "+(i+1)+"º pergunta?");
+                   
+                if(respostasIncorretas[i] > 0 && respostasIncorretas[i] < 4){
+                    for(let j=0;j<respostasIncorretas[i];j++){
+                        montarQuestoes+=`
+                        <div class="alternativa-false">
+                            <input class="inputs" type="text" id="resposta_incorreta_${j+1}" placeholder="Resposta incorreta 1" required>
+                            <input class="inputs" type="text" id="url_imagem_resposta_${j+2}" placeholder="URL da imagem 1" required>
+                        </div>`;
+                    };
+                }else{
+                    alert("Opcao Invalida");
+                    return;
+                };
+                validarPerguntas(i,respostasIncorretas[i]); 
+                montarQuestoes+=`
                 </div>
                 </div>`;
-                // if(i > 0){
-                //     document.querySelector(".subtitulo-formulario").classList.add("editor");
-                // }
-                teste=montarQuestoes;
+                
 
              };  
              montarQuestoes +=`</div>
-             <button class="botao-formulario" onclick="validarPasso2()">Prosseguir pra criar perguntas</button>
-    </section>
+             <button class="botao-formulario" onclick="validarPasso2(${qtde})">Prosseguir pra criar perguntas</button>`;
+             montarQuestoes +=`</section>
     </div>`;
 
     document.body.innerHTML +=montarQuestoes;
 
-   
+    
 }
 
 function exibirPerguntas(exibir){
@@ -147,18 +168,108 @@ function exibirPerguntas(exibir){
     exibir.parentElement.parentElement.children[1].classList.remove("ocultar");
 }
 
+function validarPerguntas(qtde_quizz,qtde_respostas_erradas){
+        //eu recebo um quiz por vez;
+       console.log(`${qtde_quizz+1} º Pergunta possue ${qtde_perguntas} erradas`);
+}
+function validarPasso2(qtde) {
+    
+    
+    let perguntas = document.getElementById("perguntas_quizz");
+    let cor=document.getElementById("cor_quizz");
+        
+    let resposta_certa_txt = document.getElementById("resposta_ok");
+    let resposta_certa_imagem= document.getElementById("url_imagem_resposta_1");
 
-function validarPasso2() {
+    let resposta_errada_txt_1 = document.getElementById("resposta_incorreta_1");
+    let resposta_errada_imagem_1 = document.getElementById("url_imagem_resposta_2");
 
+    let resposta_errada_txt_2 = document.getElementById("resposta_incorreta_2");
+    let resposta_errada_imagem_2 = document.getElementById("url_imagem_resposta_3");
 
-    let perguntas = document.getElementById("");
-    // let =document.getElementById("");
-    // let =document.getElementById("");
+    let resposta_errada_txt_3 = document.getElementById("resposta_incorreta_3");
+    let resposta_errada_imagem_3 = document.getElementById("url_imagem_resposta_4");
+  
+    perguntas.value  ="Qual o nome do primeiro Homem que Deus fez?";
+    resposta_certa_txt.value = "Adão";
+    resposta_certa_imagem.value = "https://upload.wikimedia.org/wikipedia/pt/a/ac/Vegeta.jpg";
+   
+    resposta_errada_txt_1.value = "Noé";
+    resposta_errada_imagem_1.value="https://upload.wikimedia.org/wikipedia/pt/a/ac/Vegeta.jpg";
+  
+    resposta_errada_txt_2.value = "Caim";
+    resposta_errada_imagem_2.value="https://upload.wikimedia.org/wikipedia/pt/a/ac/Vegeta.jpg";
+    resposta_errada_txt_3.value = "Abel";
+    resposta_errada_imagem_3.value="https://upload.wikimedia.org/wikipedia/pt/a/ac/Vegeta.jpg";
+
+    cor.value="#fafafa";
+    
+    if(perguntas.value.length <TAMANHOMINIMO){
+        alert("Texto invalido o texto de ser maior");
+        perguntas.value="";
+        perguntas.focus();
+        return;
+    }
+
+    if(/^#[0-9A-F]{6}$/i.test(cor)==false){
+        alert("Informe uma cor no formato hexdecimal exemplo: #F23F23");
+        cor.value = document.getElementById("favcolor").value;
+    }
+
+    if(resposta_certa_txt.value === "" ){
+        alert("Este campo não pode estar vazio");
+        resposta_certa_txt.focus();
+        return;
+    }
+
+    if ( /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(resposta_certa_imagem.value) == false ){
+        alert("Endereço de imagem invalido");    
+        resposta_certa_imagem.focus();
+        return;
+    }
+
+    
+    if(resposta_errada_txt_1.value === "" ){
+        alert("Este campo não pode estar vazio");
+        resposta_errada_txt_1.focus();
+        return;
+    }
+    if ( /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(resposta_errada_imagem_1.value) == false ){
+        alert("Endereço de imagem invalido");    
+        resposta_errada_imagem_1.focus();
+        return;
+    }
+    
+    if(resposta_errada_txt_2.value === "" ){
+        alert("Este campo não pode estar vazio");
+        resposta_errada_txt_2.focus();
+        return;
+    }
+    if ( /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(resposta_errada_imagem_2.value) == false ){
+        alert("Endereço de imagem invalido");    
+        resposta_errada_imagem_2.focus();
+        return;
+    }
+
+    if(resposta_errada_txt_3.value === "" ){
+        alert("Este campo não pode estar vazio");
+        resposta_errada_txt_3.focus();
+        return;
+    }
+    if ( /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(resposta_errada_imagem_3.value) == false ){
+        alert("Endereço de imagem invalido");    
+        resposta_errada_imagem_3.focus();
+        return;
+    }
+    
 
     alert("Tudo ok");
+
     // document.querySelector(".criar-perguntas").style.display = "none"
     // decidirNiveis();
 }
+
+
 /*
 function decidirNiveis() {
 
